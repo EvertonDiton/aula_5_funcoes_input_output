28b40e73ded9541e709196cd2283ea06a1639fbc
EvertonDiton <119082638+EvertonDiton@users.noreply.github.com>
2024-11-23 17:01:35 -0300

    Add files via upload

---
+++ aula_5.py
@@ -0,0 +1,17 @@
+print()
+print("Atividades")
+print()
+
+numero1 = float(input('Digite um numero: '))
+print('        +')
+numero2 = float(input('Digite um numero: '))
+soma = numero1 + numero2
+print()
+print('   Soma =',soma)
+print()
+
+nome = input('Digite seu nome: ')
+print()
+print('Bem-vindo!',nome)
+print()
+print()
---
+++ exercicio.py
@@ -0,0 +1,46 @@
+print()
+print('Exercícios:')
+print()
+print('1)','Olá, seja bem-vindo(a)!')
+print()
+variavel_boleana = True
+tipo = type(variavel_boleana)
+print('2)',tipo)
+print()
+multiplicacao = 2.5*4.3
+print('3)',multiplicacao)
+print()
+divisao_inteiros = 12/6
+print('4)',divisao_inteiros)
+print()
+subtracao = 20-14
+print('5)',subtracao)
+print()
+print('6)',40/10)
+print()
+print('7)',1.5*2.5*3.5*4.5)
+print()
+numero_inteiro = 120
+print('8)',numero_inteiro*2)
+print()
+print('9) Calculadora:')
+print()
+numero1 = int(input('   Digite um numero:  '))
+numero2 = int(input('   Digite um numero:  '))
+soma = numero1 + numero2
+subtr = numero1 - numero2
+multipli = numero1*numero2
+div = numero1/numero2
+print()
+print('soma', soma)
+print('Subtração', subtr)
+print('Multiplicação',multipli)
+print('Divisão', div)
+print()
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print()
+print()
+print('Seja bem-vindo(a)', nome)
+print()
+print()
